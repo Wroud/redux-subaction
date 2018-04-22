2e713999bb61ecc7855a55d3c59e0ff63951b664
Wroud <wrouds@gmail.com>
2018-04-22 14:15:04 +0300

    1.0.6-alpha2 update builds

--- es/index.js
+++ es/index.js
@@ -1,4 +1,4 @@
 export { createAction, createPayloadAction, getActionMeta, getActionCreator, getPayloadCreator, getCreators, mapToActions, mapDispatchToCreators, prepareActions, } from "./action";
-export { SubReducer, RootReducer, createRootReducer, createSubReducer, } from "./reducer";
+export { SubReducer, RootReducer, createRootReducer, createSubReducer, getState, } from "./reducer";
 export { LocalReducer, LocalListener, connectState, connectWithComponentId, resetComponentId, } from "./localReducer";
 export { Logging, LoggingLevel, } from "./config";
